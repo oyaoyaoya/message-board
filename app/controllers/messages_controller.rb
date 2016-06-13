33c53e8a7b2203a056fa30f31f3b61c7bbbea8b2
Hayato Oyanagi <14bn183n@rikkyo.ac.jp>
2016-06-13 23:09:12 +0000

    8章まで

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @message = Message.new
@@ -24,11 +24,16 @@ class MessagesController < ApplicationController
   def update
     if @message.update(message_params)
       #保存に成功した場合はトップページへリダイレクト
-      redirect_to_root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path , notice: 'メッセージを編集しました'
     else
       #保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
