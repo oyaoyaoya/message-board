e351fc7a39c9664f34d536e58b66b1448744c3f3
Hayato Oyanagi <14bn183n@rikkyo.ac.jp>
2016-06-20 14:10:03 +0000

    0621

--- db/migrate/20160615000341_add_age_to_messages.rb
+++ db/migrate/20160615000341_add_age_to_messages.rb
@@ -1,5 +1,6 @@
 class AddAgeToMessages < ActiveRecord::Migration
   def change
-    add_column :messages, :integer
+    # add_column :messages, :integer
+    add_column :messages, :age, :integer
   end
 end
